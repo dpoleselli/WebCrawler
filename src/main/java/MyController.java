36c6a49aa5e743c364c6857cd7be1aa58668bdb5
dpoleselli <dpoleselli@westmont.edu>
2019-09-21 23:53:34 -0700

    initial commit

---
+++ src/main/java/MyController.java
@@ -0,0 +1,40 @@
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class MyController {
+	public static void main(String[] args) throws Exception {
+		long time = System.currentTimeMillis();
+        String crawlStorageFolder = "/home/dpoleselli/eclipse-workspace/Crawler/data/crawl/root";
+        int numberOfCrawlers = 1;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setPolitenessDelay(100);
+        config.setUserAgentString("Westmont IR Dante Poleselli: Team Warrior");
+
+        // Instantiate the controller for this crawl.
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed("http://djp3.westmont.edu/classes/2019_08_CS128/Bible/bible.html");
+
+    	
+    	// The factory which creates instances of crawlers.
+        CrawlController.WebCrawlerFactory<MyCrawler> factory = MyCrawler::new;
+        
+        // Start the crawl. This is a blocking operation, meaning that your code
+        // will reach the line after this only when crawling is finished.
+        controller.start(factory, numberOfCrawlers);
+        
+        
+        System.out.println("Total time(sec): " + (System.currentTimeMillis() - time)/1000);
+    }
+}
---
+++ src/main/java/MyCrawler.java
@@ -0,0 +1,71 @@
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class MyCrawler extends WebCrawler {
+	
+	Data myData;
+	
+	public MyCrawler() {
+		myData = new Data();
+	}
+
+	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
+			+ "|png|mp3|mp4|zip|gz))$");
+
+	/**
+	 * This method receives two parameters. The first parameter is the page
+	 * in which we have discovered this new url and the second parameter is
+	 * the new url. You should implement this function to specify whether
+	 * the given url should be crawled or not (based on your crawling logic).
+	 * In this example, we are instructing the crawler to ignore urls that
+	 * have css, js, git, ... extensions and to only accept urls that start
+	 * with "https://www.ics.uci.edu/". In this case, we didn't need the
+	 * referringPage parameter to make the decision.
+	 */
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		return !FILTERS.matcher(href).matches()
+				&& href.startsWith("http://djp3.westmont.edu/");
+	}
+
+	/**
+	 * This function is called when a page is fetched and ready
+	 * to be processed by your program.
+	 */
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("URL: " + url);
+
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			Set<WebURL> links = htmlParseData.getOutgoingUrls();
+			
+			myData.incLinks(links.size());
+			myData.processText(url, text);
+			myData.incPages();
+
+			// We dump this crawler statistics after processing every 50 pages
+	        if ((myData.getPages() % 3) == 0) {
+	            System.out.println("Pages: " + myData.getPages());
+	            System.out.println("Links: " + myData.getLinks());
+	            System.out.println("Longest: " + myData.getLongestPage());
+	        }
+		}
+	}
+	
+	/**
+     * Return the stored data of this crawler
+     */
+    @Override
+    public Object getMyLocalData() {
+        return myData;
+    }
+}
